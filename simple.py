f80dbe4c2e5adfebff2f4481d5934fabbb91e3a0
amit <amitranpise.2011@gmail.com>
2020-07-06 18:20:54 +0530

    added simple python file

---
+++ simple.py
@@ -0,0 +1,14 @@
+i=1
+n=int(input('Enter the number'))
+for i in range(0,n):
+          a=int(input("Enter number a:"));
+          b=int(input("enter number b:"));
+          c=int(input("enter number c:"));
+          if a>b and a>c :
+               print("a is greater")
+          if b>a and b>c: 
+               print("b is greater")
+          else:
+               print("c is greater")
+           
+
